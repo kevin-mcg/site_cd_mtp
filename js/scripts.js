9aa3be5a9f86cdf3296a00cbc422494a8bde0c8f
harisris <haris.coliche@etu.univ-smb.fr>
2025-09-09 20:29:23 +0200

    Fix dropdown menu

--- js/scripts.js
+++ js/scripts.js
@@ -59,23 +59,6 @@
       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
     }
   });
-
-  /* When the user clicks on the button,
-  toggle between hiding and showing the dropdown content */
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-  }
-
 })(jQuery); // End of use strict
 
 // Fonction globale accessible depuis le HTML
